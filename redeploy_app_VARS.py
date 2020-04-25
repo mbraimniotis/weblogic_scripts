91a701d1bf4dbf065270761370011c9c8af6dca2
mbraimniotis <marios.braimniotis@gmail.com>
2020-04-25 14:59:14 +0300

    redeploy ear

---
+++ redeploy_app_VARS.py
@@ -0,0 +1,7 @@
+deploy_dir='/ear/files/directory/'
+app_undeploy_list=['FCUBSAPP', 'GWEJB', 'SCHDLR', 'FCUBSAccService', 'FCUBSCcyService', 'FCUBSCFService', 'FCUBSCLService', 'FCUBSCustomerService', 'FCUBSDDService', 'FCUBSExtPCService', 'FCUBSFTService', 'FCUBSMSService', 'FCUBSPCService', 'FCUBSRTService', 'FCUBSSIService', 'FCUBSSMService', 'FCUBSTDFinService', 'FCUBSTDService', 'FCUBSUPService']
+app_list=['FCUBSAPP', 'GWEJB', 'SCHDLR', 'FCUBSAccService', 'FCUBSCcyService', 'FCUBSCFService', 'FCUBSCLService', 'FCUBSCustomerService', 'FCUBSDDService', 'FCUBSExtPCService', 'FCUBSFTService', 'FCUBSMSService', 'FCUBSPCService', 'FCUBSRTService', 'FCUBSSIService', 'FCUBSSMService', 'FCUBSTDFinService', 'FCUBSTDService', 'FCUBSUPService']
+admin_user='username'
+deploy_target='gw_cluster'
+admin_pass='password'
+admin_url='t3s://domainname:port' 
